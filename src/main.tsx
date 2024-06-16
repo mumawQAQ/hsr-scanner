0d7ad8d0c1407375a9313a5e6e37f7bc34aa5a02
muma <GUW18@pitt.edu>
2024-06-15 21:14:32 -0400

    Add tailwindcss config and NextUI

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {NextUIProvider} from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App/>
+        <NextUIProvider>
+            <App/>
+        </NextUIProvider>
     </React.StrictMode>,
 )
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import {nextui} from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {},
+    plugins: [nextui()],
+    darkMode: "class",
+}
+
