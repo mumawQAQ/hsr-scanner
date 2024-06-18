3c7b46c81b3bbe5455116b67dc19e560d80b6860
muma <GUW18@pitt.edu>
2024-06-17 20:49:12 -0400

    Add Relic store

---
+++ src/store/relicStore.ts
@@ -0,0 +1,46 @@
+import {create} from "zustand";
+import {RelicMainStats, RelicSubStats} from "../../types.ts";
+
+type RelicStore = {
+    relicTitle: string;
+    getRelicTitle: () => string;
+    setRelicTitle: (relicTitle: string) => void;
+
+    mainRelicStats: RelicMainStats | null;
+    getMainRelicStats: () => RelicMainStats | null;
+    setMainRelicStats: (mainRelicStats: RelicMainStats | null) => void;
+
+    subRelicStats: RelicSubStats[] | [];
+    getSubRelicStats: () => RelicSubStats[] | [];
+    setSubRelicStats: (subRelicStats: RelicSubStats[]) => void;
+
+
+}
+
+const useRelicStore = create<RelicStore>((set, get) => ({
+    relicTitle: "",
+    getRelicTitle: () => {
+        return get().relicTitle;
+    },
+    setRelicTitle: (relicTitle) => {
+        set({relicTitle});
+    },
+
+    mainRelicStats: null,
+    getMainRelicStats: () => {
+        return get().mainRelicStats;
+    },
+    setMainRelicStats: (mainRelicStats) => {
+        set({mainRelicStats});
+    },
+
+    subRelicStats: [],
+    getSubRelicStats: () => {
+        return get().subRelicStats;
+    },
+    setSubRelicStats: (subRelicStats) => {
+        set({subRelicStats});
+    }
+}));
+
+export default useRelicStore;
