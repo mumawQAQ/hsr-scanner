951aa02bef895fee938b762e14926da046470175
muma <GUW18@pitt.edu>
2024-06-25 19:51:50 -0400

    Fix the BrowserRouter can't work after built, change to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import { NavigationItem } from '@/components/navigation/navigation-item.tsx';
