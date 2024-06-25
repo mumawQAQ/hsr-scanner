da3dd4f8d7cf28555677b701b9ac3cc29736686c
kroulis <qinkroulis@gmail.com>
2024-06-25 14:28:24 -0700

    CRLF to LF

--- src/App.tsx
+++ src/App.tsx
@@ -212,7 +212,6 @@ function App() {
 
   const captureScreen = async () => {
     const res = await (window as any).ipcRenderer.captureScreen();
-    
     const croppedImage = res.crop({
       x: 1400,
       y: 0,
@@ -229,7 +228,7 @@ function App() {
     setIsLoaded(false);
     // reset the stats
     resetAttributes();
-    setCurrentImage(croppedImage);
+    setCurrentImage(croppedImage.toDataURL());
 
     try {
       // source image
