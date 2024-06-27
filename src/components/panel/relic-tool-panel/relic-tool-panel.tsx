eda35f604118db85fb8dae5ba7f1255cb53a17d8
muma <GUW18@pitt.edu>
2024-06-27 17:35:02 -0400

    Add the relic tool panel

---
+++ src/components/panel/relic-tool-panel/relic-tool-panel.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+
+import RelicRuleCreate from '@/components/panel/relic-tool-panel/relic-rule-create.tsx';
+import RelicRuleEdit from '@/components/panel/relic-tool-panel/relic-rule-edit.tsx';
+import RelicRuleTemplateList from '@/components/panel/relic-tool-panel/relic-rule-template-list.tsx';
+import RelicToolNavbar from '@/components/panel/relic-tool-panel/relic-tool-navbar.tsx';
+
+const RelicToolPanel = () => {
+  return (
+    <div>
+      <RelicToolNavbar />
+      <Routes>
+        <Route index element={<RelicRuleTemplateList />} />
+        <Route path="edit/:templateId" element={<RelicRuleEdit />} />
+        <Route path="create/:templateName/:templateDescription/:author" element={<RelicRuleCreate />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default RelicToolPanel;
