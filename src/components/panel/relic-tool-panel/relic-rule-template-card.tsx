e20213bb1ee8e33064e578b2253c54573f66e129
muma <GUW18@pitt.edu>
2024-06-27 22:30:29 -0400

    Fix the route of createEdit

--- src/components/panel/relic-tool-panel/relic-rule-template-card.tsx
+++ src/components/panel/relic-tool-panel/relic-rule-template-card.tsx
@@ -1,21 +1,35 @@
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card.tsx';
 import { X } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
+
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card.tsx';
+import useRelicTemplateStore from '@/hooks/use-relic-template-store.ts';
 
 type RelicRuleTemplateCardProps = {
   name: string;
   description: string;
+  templateID: string;
 };
 
-const RelicRuleTemplateCard = ({ name, description }: RelicRuleTemplateCardProps) => {
+const RelicRuleTemplateCard = ({ name, description, templateID }: RelicRuleTemplateCardProps) => {
+  const { setCurrentRelicRatingRulesTemplate, relicRatingRulesTemplateStore } = useRelicTemplateStore();
   const navigate = useNavigate();
-  const templateID = '1';
+
+  if (!relicRatingRulesTemplateStore) {
+    return null;
+  }
+
+  if (!relicRatingRulesTemplateStore[templateID]) {
+    return null;
+  }
 
   return (
     <Card
       className="h-fit cursor-pointer hover:ring hover:ring-offset-1"
       onClick={() => {
-        navigate(`edit/${templateID}`);
+        // set the current template
+        setCurrentRelicRatingRulesTemplate(relicRatingRulesTemplateStore[templateID]);
+
+        navigate(`createEdit/${templateID}`);
       }}
     >
       <CardHeader className="relative">
