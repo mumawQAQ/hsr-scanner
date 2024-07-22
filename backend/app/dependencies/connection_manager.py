e4007e4b8b00a81317fc83e23ca0416bde810c4c
muma <GUW18@pitt.edu>
2024-07-22 14:39:20 -0400

    Add connection manager

---
+++ backend/app/dependencies/connection_manager.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from fastapi.websockets import WebSocket
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+def get_connection_manager():
+    return manager
--- backend/app/routers/websocket.py
+++ backend/app/routers/websocket.py
@@ -1,11 +1,18 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, Depends, WebSocketDisconnect
+
+from app.dependencies.connection_manager import ConnectionManager, get_connection_manager
 
 router = APIRouter()
 
 
 @router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+async def websocket_endpoint(websocket: WebSocket, manager: ConnectionManager = Depends(get_connection_manager)):
+    await manager.connect(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_message(data)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
