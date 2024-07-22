6616140109dc1b95e9d406b3597a70f7e3cb4a43
muma <GUW18@pitt.edu>
2024-07-22 14:19:59 -0400

    When server started, write to a JSON about it host and port

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,30 @@
+import uvicorn
 from fastapi import FastAPI
+from uvicorn import Server
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def server_started(server):
+    for server in server.servers:
+        for socket in server.sockets:
+            # write to a json file about the host and port
+            with open('server.json', 'w') as f:
+                f.write(f'{{"host": "{socket.getsockname()[0]}", "port": {socket.getsockname()[1]}}}')
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == '__main__':
+    config = uvicorn.Config(app, port=0)
+    server: Server = uvicorn.Server(config)
+
+    orig_log_started_message = server._log_started_message
+
+
+    def patch_log_started_message(listeners):
+        orig_log_started_message(listeners)
+        server_started(server)
+
+
+    server._log_started_message = patch_log_started_message
+
+    server.run()
