c2fba65fb022f80ad0e7c2d61494a494bbcf8e46
muma <GUW18@pitt.edu>
2024-07-24 18:11:54 -0400

    Format the logger with dict config

--- backend/app/logging_config.py
+++ backend/app/logging_config.py
@@ -1,12 +1,41 @@
 import logging
-import sys
-from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(level=logging.INFO,
-                    format="%(asctime)s [%(levelname)s] %(message)s",
-                    handlers=[
-                        RotatingFileHandler("app.log", maxBytes=1000000, backupCount=5, encoding='utf-8'),
-                        logging.StreamHandler(sys.stdout)
-                    ])
+# Define the logging configuration
+logging_config = dict({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'detailed': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s (%(filename)s:%(lineno)d): %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'WARNING',
+            'formatter': 'detailed',
+            'stream': 'ext://sys.stdout',
+        },
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'formatter': 'detailed',
+            'filename': 'app.log',
+            'mode': 'a',
+            'maxBytes': 1000000,  # 1MB
+            'backupCount': 5,
+            'encoding': 'utf-8',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    }
+})
 
+# Apply the logging configuration
+logging.config.dictConfig(logging_config)
 logger = logging.getLogger(__name__)
