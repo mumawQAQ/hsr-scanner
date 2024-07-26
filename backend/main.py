ea7c9021f29ee601c1e35df3d142d3ed35002409
muma <GUW18@pitt.edu>
2024-07-26 15:49:39 -0400

    No longer need to write backend server port from file

--- backend/main.py
+++ backend/main.py
@@ -11,27 +11,7 @@ app = FastAPI(lifespan=life_span)
 app.include_router(websocket.router)
 app.include_router(state.router)
 
-
-def server_started(server):
-    for server in server.servers:
-        for socket in server.sockets:
-            # write to a json file about the host and port
-            with open('server.json', 'w') as f:
-                f.write(f'{{"host": "{socket.getsockname()[0]}", "port": {socket.getsockname()[1]}}}')
-
-
 if __name__ == '__main__':
     config = uvicorn.Config(app, port=0)
     server: Server = uvicorn.Server(config)
-
-    orig_log_started_message = server._log_started_message
-
-
-    def patch_log_started_message(listeners):
-        orig_log_started_message(listeners)
-        server_started(server)
-
-
-    server._log_started_message = patch_log_started_message
-
     server.run()
