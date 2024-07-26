ade6711a9045a9346650474b3f162124d0d5acc7
muma <GUW18@pitt.edu>
2024-07-26 00:10:02 -0400

    Add color to console logger

--- backend/app/logging_config.py
+++ backend/app/logging_config.py
@@ -1,11 +1,40 @@
 import logging
 
+
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;21m"
+    green = "\x1b[32;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    base_format = "%(asctime)s [%(levelname)s] %(name)s (%(filename)s:%(lineno)d): %(message)s"
+
+    LEVEL_COLORS = {
+        logging.DEBUG: grey,
+        logging.INFO: green,
+        logging.WARNING: yellow,
+        logging.ERROR: red,
+        logging.CRITICAL: bold_red
+    }
+
+    def format(self, record):
+        # Apply color only to the levelname part
+        log_fmt = self.base_format.replace("[%(levelname)s]",
+                                           self.LEVEL_COLORS[record.levelno] + "[%(levelname)s]" + self.reset)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 # Define the logging configuration
-logging_config = dict({
+logging_config = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
         'detailed': {
+            '()': CustomFormatter
+        },
+        'standard': {
             'format': '%(asctime)s [%(levelname)s] %(name)s (%(filename)s:%(lineno)d): %(message)s'
         },
     },
@@ -19,7 +48,7 @@ logging_config = dict({
         'file': {
             'class': 'logging.handlers.RotatingFileHandler',
             'level': 'ERROR',
-            'formatter': 'detailed',
+            'formatter': 'standard',
             'filename': 'app.log',
             'mode': 'a',
             'maxBytes': 1000000,  # 1MB
@@ -34,6 +63,6 @@ logging_config = dict({
             'propagate': True
         },
     }
-})
+}
 
 logger = logging.getLogger(__name__)
