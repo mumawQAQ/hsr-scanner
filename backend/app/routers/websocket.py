e91384d1d9fde4af6a4a35fc1f692be8090251c2
muma <GUW18@pitt.edu>
2024-07-26 20:20:38 -0400

    Prevent the websocket sending duplicate messages

--- backend/app/routers/websocket.py
+++ backend/app/routers/websocket.py
@@ -1,3 +1,5 @@
+import asyncio
+import json
 from typing import Annotated
 
 from fastapi import APIRouter, WebSocket, Depends, WebSocketDisconnect
@@ -8,7 +10,7 @@ from app.dependencies.global_state import GlobalState, get_global_state
 router = APIRouter()
 
 
-@router.websocket("/ws")
+@router.websocket("/relic-info")
 async def websocket_endpoint(websocket: WebSocket,
                              manager: Annotated[ConnectionManager, Depends(get_connection_manager)],
                              global_state: Annotated[GlobalState, Depends(get_global_state)]):
@@ -16,7 +18,19 @@ async def websocket_endpoint(websocket: WebSocket,
 
     try:
         while True:
-            data = await websocket.receive_text()
-            await manager.send_message(data)
+            if not global_state.relic_info:
+                error_message = json.dumps({'status': 'error', 'message': 'No relic info found'})
+                if manager.relic_info_last_sent_message != error_message:
+                    manager.relic_info_last_sent_message = error_message
+                    await manager.send_message(error_message)
+                await asyncio.sleep(0.1)
+                continue
+
+            current_message = global_state.relic_info.model_dump_json()
+            if manager.relic_info_last_sent_message != current_message:
+                manager.relic_info_last_sent_message = current_message
+                await manager.send_message(current_message)
+            await asyncio.sleep(0.1)
+
     except WebSocketDisconnect:
         manager.disconnect(websocket)
