f3af67b2a16b4d166e32513a772927173a2a0c32
muma <GUW18@pitt.edu>
2024-07-26 20:18:52 -0400

    Add cors

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import Server
 
 from app.life_span import life_span
-from app.routers import websocket, state
+from app.routers import state
+from app.routers import websocket
 
 app = FastAPI(lifespan=life_span)
 
-# include all the routers here
-app.include_router(websocket.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(state.router)
+app.include_router(websocket.router)
 
 if __name__ == '__main__':
     config = uvicorn.Config(app, port=0)
