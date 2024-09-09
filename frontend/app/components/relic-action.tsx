1653a30e6be12856cf9459d53243b36cb8b5c5b1
muma <wgr5600133@gmail.com>
2024-09-08 19:18:34 -0700

    Implement the start scan function

--- frontend/app/components/relic-action.tsx
+++ frontend/app/components/relic-action.tsx
@@ -1,13 +1,22 @@
+'use client';
 import { Switch } from '@nextui-org/switch';
 import { FileLock, Image, ListEnd, ScanEye } from 'lucide-react';
 import { Button } from '@nextui-org/button';
 import useWindowStore from '@/app/hooks/use-window-store';
+import useBackendClientStore from '@/app/hooks/use-backend-client-store';
 
 export default function RelicAction() {
   const { setScanningStatus, scanningStatus, imgShow, setImageShow, logPause, setLogPause } = useWindowStore();
+  const { patch } = useBackendClientStore();
+
+  const handleScanStateChange = async (status: boolean) => {
+    await patch('scan-state', status.toString());
+    setScanningStatus(status);
+  };
+
   return (
     <div className="flex flex-col gap-4">
-      <Switch color="success" thumbIcon={<ScanEye />} isSelected={scanningStatus} onValueChange={setScanningStatus}>
+      <Switch color="success" thumbIcon={<ScanEye />} isSelected={scanningStatus} onValueChange={handleScanStateChange}>
         开始扫描
       </Switch>
       <Switch color="success" thumbIcon={<Image />} isSelected={imgShow} onValueChange={setImageShow}>
