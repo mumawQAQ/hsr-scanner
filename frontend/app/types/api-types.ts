53077c1c77b7bc1092453c0009b142aab6ca23f3
mumawQAQ <GUW18@pitt.edu>
2024-09-15 19:08:01 -0700

    Add create template request type

--- frontend/app/types/api-types.ts
+++ frontend/app/types/api-types.ts
@@ -3,3 +3,10 @@ export type ApiResponse<T> = {
   message: string; // this only exists when status is 'error'
   data?: T; // this only exists when status is 'success'
 };
+
+export type CreateRelicTemplateRequest = {
+  id: string;
+  name: string;
+  description: string;
+  author: string;
+};
