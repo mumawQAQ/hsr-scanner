d8f639a509ca65f05d69787b5360f2cd3a7c1674
mumawQAQ <GUW18@pitt.edu>
2024-11-17 20:05:23 -0800

    chore(frontend): remove qr code import template

--- frontend/app/components/modal/import-template-modal.tsx
+++ frontend/app/components/modal/import-template-modal.tsx
@@ -1,11 +1,9 @@
 import { useModal } from '@/app/hooks/use-modal-store';
 import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from '@nextui-org/modal';
 import { Button } from '@nextui-org/button';
-import DragAndDrop from '@/app/components/drag-and-drop';
 import { useImportTemplate } from '@/app/apis/relic-template';
 import toast from 'react-hot-toast';
 import { Textarea } from '@nextui-org/input';
-import { Divider } from '@nextui-org/divider';
 import { useState } from 'react';
 
 export const ImportTemplateModal = () => {
@@ -13,20 +11,6 @@ export const ImportTemplateModal = () => {
   const { isOpen, onClose, onOpen, type } = useModal();
   const importTemplate = useImportTemplate();
   const isModalOpen = isOpen && type === 'import-template';
-
-  const handleQrCodeData = (data: string | null) => {
-    if (!data) {
-      return;
-    }
-    importTemplate.mutate(data, {
-      onSuccess: () => {
-        onOpen('select-template');
-      },
-      onError: () => {
-        toast.error('导入模板失败，请检查二维码是否正确, 或联系作者反馈');
-      },
-    });
-  };
 
   const handleImportTemplateCode = () => {
     if (!templateCode) {
@@ -45,10 +29,8 @@ export const ImportTemplateModal = () => {
   return (
     <Modal isOpen={isModalOpen} onClose={onClose}>
       <ModalContent className="flex items-center justify-center p-6">
-        <ModalHeader>请上传二维码或输入模板代码以导入模板</ModalHeader>
+        <ModalHeader>请输入模板代码以导入模板</ModalHeader>
         <ModalBody className="flex">
-          <DragAndDrop onQRCodeData={handleQrCodeData} />
-          <Divider />
           <Textarea
             maxRows={2}
             label="模板代码"
