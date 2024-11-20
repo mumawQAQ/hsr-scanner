e39dd8c21dfa32734f59eda0cb6ff11a3b8f2846
mumawQAQ <GUW18@pitt.edu>
2024-11-19 22:16:49 -0800

    fix(backend): fix icon detection with yolo model

--- backend/app/core/pipeline_stage_impls/relic_discard_stage.py
+++ backend/app/core/pipeline_stage_impls/relic_discard_stage.py
@@ -1,11 +1,11 @@
 import pyautogui as pg
 
-from app.core.data_models.icon_matcher import IconMatcherOutput, IconMatcherInput, IconType
 from app.core.data_models.pipeline_context import PipelineContext
 from app.core.data_models.stage_enums import GameRecognitionStage
 from app.core.data_models.stage_result import StageResult
 from app.core.interfaces.base.base_pipeline_stage import BasePipelineStage
 from app.core.managers.model_manager import ModelManager
+from app.logging_config import logging
 
 
 class RelicDiscardStage(BasePipelineStage):
@@ -15,17 +15,13 @@ class RelicDiscardStage(BasePipelineStage):
     async def process(self, context: PipelineContext) -> StageResult:
         try:
             screenshot = context.data.get(GameRecognitionStage.SCREENSHOT.value)
+            detection = context.data.get(GameRecognitionStage.DETECTION.value)
             relic_analysis = context.data.get(GameRecognitionStage.RELIC_ANALYSIS.value)
             relic_discard_score = context.meta_data.get("relic_discard_score", 0.4)
             skip_if_error = context.meta_data.get("skip_if_error", True)
 
-            icon_matcher_model = ModelManager().get_model("icon_matcher")
-
             # TODO: this may need to move to a separate stage
             keyboard_model = ModelManager().get_model("keyboard")
-
-            if not icon_matcher_model:
-                raise ValueError("Icon matcher model not found.")
 
             if not keyboard_model:
                 raise ValueError("Keyboard model not found.")
@@ -38,16 +34,16 @@ class RelicDiscardStage(BasePipelineStage):
                     keyboard_model.predict("d")
                 raise ValueError("Relic analysis data not found.")
 
-            if len(relic_analysis) == 0 or relic_analysis[0].score < relic_discard_score:
-                icon_center_info: IconMatcherOutput = icon_matcher_model.predict(
-                    IconMatcherInput(
-                        source_image_bgr=screenshot['image_bgr'],
-                        icon_type=IconType.DISCARD
-                    )
-                )
+            if detection is None or 'discard-icon' not in detection:
+                if skip_if_error:
+                    keyboard_model.predict("d")
+                raise ValueError("Detection discard icon data not found.")
 
-                icon_center_x = icon_center_info.x_center
-                icon_center_y = icon_center_info.y_center
+            if len(relic_analysis) == 0 or relic_analysis[0].score < relic_discard_score:
+                icon_center_x = detection['discard-icon']['box']['x_center']
+                icon_center_y = detection['discard-icon']['box']['y_center']
+
+                logging.info(f"Discard icon center: ({icon_center_x}, {icon_center_y})")
 
                 window_left = screenshot['window']['left']
                 window_top = screenshot['window']['top']
@@ -55,8 +51,12 @@ class RelicDiscardStage(BasePipelineStage):
                 icon_x = window_left + icon_center_x
                 icon_y = window_top + icon_center_y
 
+                logging.info(f"Discard icon screen position: ({icon_x}, {icon_y})")
+
                 # click on the icon
                 pg.click(icon_x, icon_y)
+                # reset mouse position
+                pg.moveTo(1, 1)
 
             keyboard_model.predict("d")
 
