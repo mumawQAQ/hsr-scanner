21e9abc523167e805bee6dd23d1e74d5e14f032f
mumawQAQ <GUW18@pitt.edu>
2024-12-27 21:12:58 -0800

    chore: add config table

--- backend/app/core/utils/database.py
+++ backend/app/core/utils/database.py
@@ -1,11 +1,12 @@
 from app.core.database import db
+from app.core.orm_models.config_orm import ConfigORM
 from app.core.orm_models.rating_rule_orm import RatingRuleORM
 from app.core.orm_models.rating_template_orm import RatingTemplateORM
 
 
 def init_db():
     db.connect()
-    db.create_tables([RatingTemplateORM, RatingRuleORM], safe=True)
+    db.create_tables([RatingTemplateORM, RatingRuleORM, ConfigORM], safe=True)
 
 
 def close_db():
