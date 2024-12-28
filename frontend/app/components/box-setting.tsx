d2cda2f23b3770b8c9fd1b23cee1fe686dc8b6e8
mumawQAQ <GUW18@pitt.edu>
2024-12-27 21:58:38 -0800

    chore: remove unused code

--- frontend/app/components/box-setting.tsx
+++ frontend/app/components/box-setting.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Input } from '@nextui-org/input';
 import { Button } from '@nextui-org/button';
 import toast from 'react-hot-toast';
