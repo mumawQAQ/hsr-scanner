749435f2f095bc2f03a52d87012bbf9d126fe49c
mumawQAQ <GUW18@pitt.edu>
2025-01-05 09:30:41 -0800

    chore: update log output to stdout

--- backend/app/logging_config.py
+++ backend/app/logging_config.py
@@ -7,7 +7,7 @@ def set_log_level(log_level):
     logger.configure(
         handlers=[
             {
-                "sink": sys.stderr,
+                "sink": sys.stdout,
                 "level": log_level,
                 "format": "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level:<8}</level> | <cyan>{name}:{function}:{line}</cyan> - <level>{message}</level>",
                 "colorize": True,
