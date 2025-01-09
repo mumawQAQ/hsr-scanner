ee91c2d83ede93b26a9145bc058458df751e3646
mumawQAQ <GUW18@pitt.edu>
2025-01-09 14:03:08 -0800

    chore: fix some recursion error

--- backend/app/core/state_machines/pipeline_state_machine.py
+++ backend/app/core/state_machines/pipeline_state_machine.py
@@ -34,6 +34,7 @@ class PipelineStateMachine:
             transitions=pipeline_config["transitions"],
             initial=pipeline_config["initial"],
             auto_transitions=False,  # Disable auto transitions to prevent unexpected behavior
+            ignore_invalid_triggers=True  # Ignore invalid triggers
         )
 
     async def run_current_stage(self):
@@ -57,7 +58,7 @@ class PipelineStateMachine:
                                 data=result.data,
                                 pipeline_id=self.context.pipeline_id,
                                 pipeline_type=self.config_name
-                            ).model_dump_json(),
+                            ).model_dump(),
                             to=self.sid
                         )
 
@@ -78,40 +79,37 @@ class PipelineStateMachine:
                             error=result.error,
                             pipeline_id=self.context.pipeline_id,
                             pipeline_type=self.config_name
-                        ).model_dump_json(),
+                        ).model_dump(),
                         to=self.sid
                     )
                     logger.error(
                         f"[{self.config_name}:{self.context.pipeline_id}] Error while running {self.state} stage."
                     )
 
-                    if result.metadata.stop_when_failed:
-                        logger.info(
-                            f"[{self.config_name}:{self.context.pipeline_id}] Stopping pipeline due to stage failure."
-                        )
-                        await self.stop_pipeline_trigger()
-
                     await self.on_error_trigger()
             else:
                 logger.info(f"[{self.config_name}:{self.context.pipeline_id}] Pipeline has been cancelled.")
-                await self.sio.emit(
-                    "pipeline_stopped",
-                    {"pipeline_id": self.context.pipeline_id},
-                    to=self.sid
-                )
         except Exception:
             logger.exception(
                 f"[{self.config_name}:{self.context.pipeline_id}] Error while running {self.state} stage."
             )
             await self.on_error_trigger()
 
-    async def run_pipeline(self):
+    async def handle_run_pipeline(self):
         await self.sio.emit(
             "pipeline_started",
-            {"pipeline_id": self.context.pipeline_id},
+            {"pipeline_id": self.context.pipeline_id, "pipeline_type": self.config_name},
             to=self.sid
         )
         await self.start_pipeline_trigger()
 
-    async def stop_pipeline(self):
+    async def handle_stop_pipeline(self):
+        if self.state == "idle":
+            return
+        
+        await self.sio.emit(
+            "pipeline_stopped",
+            {"pipeline_id": self.context.pipeline_id, "pipeline_type": self.config_name},
+            to=self.sid
+        )
         await self.stop_pipeline_trigger()
