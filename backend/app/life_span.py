b341c7749f67a17b40a410f086d48f4f7d6ee87f
mumawQAQ <GUW18@pitt.edu>
2025-01-09 11:36:38 -0800

    chore: remove unused code and init config file

--- backend/app/life_span.py
+++ backend/app/life_span.py
@@ -1,47 +1,26 @@
+import json
+import os
 from contextlib import asynccontextmanager
-from typing import Optional
+from typing import Optional, Dict, Any
 
 from fastapi import FastAPI
+from loguru import logger
 
+from app.constant import ROOT_PATH
 from app.core.managers.global_state_manager import GlobalStateManager
 from app.core.managers.model_manager import ModelManager
-from app.core.managers.pipeline_manager import PipelineManager
 from app.core.managers.stage_manager import StageManager
-from app.core.managers.websocket_manager import WebsocketManager
-from app.core.pipeline_executer import PipelineExecutor
-from app.core.pipline_impls.auto_relic_analysis_pipeline import AutoRelicAnalysisPipeline
-from app.core.pipline_impls.single_relic_analysis_pipeline import SingleRelicAnalysisPipeline
 from app.core.utils import database
 from app.core.utils.formatter import Formatter
 from app.core.utils.template_en_decode import TemplateEnDecoder
 from app.logging_config import set_log_level
 
-websocket_manager: Optional[WebsocketManager] = None
-pipeline_manager: Optional[PipelineManager] = None
 model_manager: Optional[ModelManager] = None
-pipeline_executor: Optional[PipelineExecutor] = None
 formatter: Optional[Formatter] = None
 template_en_decoder: Optional[TemplateEnDecoder] = None
 global_state_manager: Optional[GlobalStateManager] = None
 stage_manager: Optional[StageManager] = None
-
-
-def get_websocket_manager() -> WebsocketManager:
-    if websocket_manager is None:
-        raise RuntimeError("WebsocketManager not initialized")
-    return websocket_manager
-
-
-def get_pipeline_manager() -> PipelineManager:
-    if pipeline_manager is None:
-        raise RuntimeError("PipelineManager not initialized")
-    return pipeline_manager
-
-
-def get_pipeline_executor() -> PipelineExecutor:
-    if pipeline_executor is None:
-        raise RuntimeError("PipelineExecutor not initialized")
-    return pipeline_executor
+state_machine_config: Dict[str, Any] = {}
 
 
 def get_model_manager() -> ModelManager:
@@ -74,6 +53,12 @@ def get_stage_manager() -> StageManager:
     return stage_manager
 
 
+def get_state_machine_config() -> Dict[str, Any]:
+    if state_machine_config is None:
+        raise RuntimeError("State machine config not initialized")
+    return state_machine_config
+
+
 @asynccontextmanager
 async def life_span(app: FastAPI):
     global websocket_manager
@@ -84,8 +69,17 @@ async def life_span(app: FastAPI):
     global template_en_decoder
     global global_state_manager
     global stage_manager
+    global state_machine_config
 
     set_log_level("INFO")
+
+    logger.info("Initializing the backend...")
+
+    logger.info("Loading state machine configs...")
+    # Init pipeline configs
+    with open(os.path.join(ROOT_PATH, "assets", "configs", "state_machine_config.json"), 'r',
+              encoding='utf-8') as f:
+        state_machine_config = json.load(f)
 
     # Init the template en decoder
     template_en_decoder = TemplateEnDecoder()
@@ -95,15 +89,8 @@ async def life_span(app: FastAPI):
 
     # Init the managers
     global_state_manager = GlobalStateManager()
-    websocket_manager = WebsocketManager()
-    pipeline_manager = PipelineManager()
     model_manager = ModelManager()
     stage_manager = StageManager()
-    pipeline_executor = PipelineExecutor(websocket_manager)
-
-    # Register the pipelines
-    pipeline_manager.register_pipeline(SingleRelicAnalysisPipeline)
-    pipeline_manager.register_pipeline(AutoRelicAnalysisPipeline)
 
     # Init the database
     database.init_db()
